e753c10347c9607e43fc0f9e9a6825e789e5cf6e
ima <ima.fyp.2020@gmail.com>
2020-10-27 19:08:04 +0500

    Basic version working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,21 +7,29 @@ import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgcodecs.*;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.temporal.TemporalAmount;
+import java.util.*;
+import java.util.List;
 
 public class Main {
 
-
-
     static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
-    public static void main(String[] args) {
-        String filepath = "images/colors.jpg"; //"images/colors.jpg"
+    public static void main(String[] args) throws IOException {
+
+        String filepath = "images/face1.jpg";
+        //String filepath = "images/face2.jpg";
+
+        int clusters = 5;
 
         Mat colors= new Mat();
         Mat labels = new Mat();
         TermCriteria criteria = new TermCriteria(TermCriteria.EPS+TermCriteria.COUNT,10, 1.0);
-
-        int clusters = 3;
 
         //Reading Image
         Mat img = Imgcodecs.imread(filepath);
@@ -41,8 +49,89 @@ public class Main {
         String dump = colors.dump();
         System.out.println(dump);
 
-        //Showing Image
-        //HighGui.imshow("img",img);
-        //HighGui.waitKey(0);
+        int row = 0;
+        int col = 0;
+        int element = 0;
+
+        float r = 0,g = 0,b = 0;
+
+        char[] charList = new char[100];
+
+        Color[] colorsArray = new Color[clusters];
+
+        for(int i = 0; i<dump.length(); i++){
+            if(dump.charAt(i)=='['){
+                continue;
+            }
+
+            if(dump.charAt(i) == ',')
+            {
+                String s = new String(charList).trim();
+                System.out.println(s);
+                if(element==0) {
+                    r = Math.round(Float.parseFloat(s));
+                }else if(element == 1){
+                    g = Math.round(Float.parseFloat(s));
+                }
+                else if(element == 2){
+                    b = Float.parseFloat(s);
+                }
+                charList = new char[100];
+                col = 0;
+                element++;
+                continue;
+            }
+            if(dump.charAt(i) == ';' || dump.charAt(i) == ']' ){
+                if(element == 2){
+                    String s = new String(charList).trim();
+                    System.out.println(s);
+                    b = Math.round(Float.parseFloat(s));
+                }
+                System.out.println(r+" "+g+" "+b+"");
+
+
+
+                colorsArray[row] = new Color((int)r,(int)g,(int)b,255);
+
+                row++;
+                element = 0;
+                charList = new char[100];
+                col = 0;
+                continue;
+            }
+//            if(dump.charAt(i) == '\n')
+//                continue;
+
+            charList[col]=dump.charAt(i);
+
+            col++;
+        }
+
+
+        JFrame f = new JFrame();
+
+
+        JPanel[] panels = new JPanel[clusters];
+
+        for (int i = 0; i<clusters; i++)
+        {
+            panels[i] = new JPanel();
+            panels[i].setBounds(0,100*i,100,100);
+            panels[i].setBackground(colorsArray[i]);
+            panels[i].setVisible(true);
+            f.add(panels[i]);
+        }
+
+        BufferedImage myPicture = ImageIO.read(new File(filepath));
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+        picLabel.setBounds(100,0,myPicture.getWidth(),myPicture.getHeight());
+
+        int height = Math.max(myPicture.getHeight(),clusters*100);
+
+        f.setSize(myPicture.getWidth()+100,height);
+        f.setTitle("Color Tones");
+        f.add(picLabel);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
